1688a34ab8df2b14be44ae81a1fcc8700c0e9118
HHK <hhk.contact@protonmail.com>
2021-10-21 18:51:49 +0200

    feat: rename + improve contract

--- src/SushiChan.sol
+++ src/SushiChan.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/token/erc721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@chainlink/contracts/src/v0.8/VRFConsumerBase.sol";
 
-contract InternNft is ERC721Enumerable, VRFConsumerBase, Ownable {
+contract SushiChan is ERC721Enumerable, VRFConsumerBase, Ownable {
 
     bytes32 internal keyHash;
     uint256 internal fee;
@@ -14,11 +14,11 @@ contract InternNft is ERC721Enumerable, VRFConsumerBase, Ownable {
     string public prefixURI;
     uint256 public constant MAX_SUPPLY = 1000;
 
-    constructor(string memory _prefixURI)
-    ERC721("Intern-nfts", "INFT")
+    constructor(address _vrfCoordinator, address _linkToken, string memory _prefixURI)
+    ERC721("Sushi-chan", "SCHAN")
     VRFConsumerBase(
-        0x3d2341ADb2D31f1c5530cDC622016af293177AE0, // VRF Coordinator for polygon
-        0xb0897686c545045aFc77CF20eC7A532E3120E0F1  // LINK Token for polygon
+        _vrfCoordinator, //0x3d2341ADb2D31f1c5530cDC622016af293177AE0 VRF Coordinator for polygon
+        _linkToken  //0xb0897686c545045aFc77CF20eC7A532E3120E0F1 LINK Token for polygon
     )
     {
         keyHash = 0xf86195cf7690c55907b2b611ebb7343a6f649bff128701cc542f0569e2c549da; //key hash for polygon
@@ -42,47 +42,70 @@ contract InternNft is ERC721Enumerable, VRFConsumerBase, Ownable {
         randomNumber = randomness;
     }
 
+    /**
+    * Withdraw all Link token to owner
+    */
     function withdrawLinkTokens() external onlyOwner {
         LINK.transfer(owner(), LINK.balanceOf(address(this)));
     }
 
+    /**
+    * Mint for multiple addresses in a single transaction 
+    */
     function mintMultiple(address[] memory _to) external onlyOwner {
         for (uint256 i; i < _to.length; i+=1) {
-            mint(_to[i], i);
+            mint(_to[i]);
         }
     }
 
-    function mint(address _to, uint256 _tokenId) public onlyOwner {
-        require(randomNumber == 0, "Can mint only if random not set"); //Can't cheat
-        require(totalSupply() <= MAX_SUPPLY, "All nft already minted");
-        _safeMint(_to, _tokenId);
+    /**
+    * Mint for 1 address
+    */
+    function mint(address _to) public onlyOwner {
+        require(randomNumber == 0, "Can't mint once random number is set"); //Can't cheat
+        require(totalSupply() < MAX_SUPPLY, "All nft already minted");
+        _safeMint(_to, totalSupply());
     }
 
+    /**
+    * Return tokenURI
+    */
     function tokenURI(uint256 _tokenId) public view override returns (string memory) {
         require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
+        if (randomNumber == 0) {
+            return string(abi.encodePacked(prefixURI, "nrs")); //not ready ser
+        }
  
         uint256 attributes = tokenAttributes(_tokenId);
-        return bytes(prefixURI).length > 0 ? string(abi.encodePacked(prefixURI, attributes)) : "";
+        return string(abi.encodePacked(prefixURI, attributes));
     }
     
+    /**
+    * Return token attributes
+    */
     function tokenAttributes(uint256 _tokenId) public view returns (uint256) {
         require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
-        uint256 sushi = randomAttribute(_tokenId, "character");
-        if (sushi == 9) sushi = randomAttribute(_tokenId, "this sushi looks rare");
-        
-        uint256 background = randomAttribute(_tokenId, "character");
-        if (background == 9) background = randomAttribute(_tokenId, "this background looks rare");
 
-        uint256 character = randomAttribute(_tokenId, "character");
-        if (character == 9) character = randomAttribute(_tokenId, "this character looks rare");
-        return sushi * 100 + background * 10 + character;
+        uint256 background = random(_tokenId, "background");
+        uint256 character = random(_tokenId, "character");
+        uint256 sushi = random(_tokenId, "sushi");
+
+        return background * 100 + character * 10 + sushi;
     }
 
-    function randomAttribute(uint256 _tokenId, string memory _attr) public view returns (uint256) {
+    /**
+    * Return random attribute between 0 and 9
+    */
+    function random(uint256 _tokenId, string memory _attr) public view returns (uint256) {
         require(_exists(_tokenId), "ERC721Metadata: URI query for nonexistent token");
-        return uint256(keccak256(abi.encode(randomNumber, _tokenId, _attr))) % 10;
+
+        uint256 randomUint = uint256(keccak256(abi.encode(randomNumber, _tokenId, _attr)));
+        return randomUint % 2 == 1 ? randomUint % 8 : randomUint % 9; //9 is the rarest id
     }
 
+    /**
+    * Set prefixURI 
+    */
     function setPrefixURI(string memory _prefixURI) public onlyOwner {
         prefixURI = _prefixURI;
     }
