3b591accf9ad4f2a88eebb3c2f2f234c5780c7d0
HHK <hhk.contact@protonmail.com>
2021-10-21 18:52:19 +0200

    feat: add tests

---
+++ src/test/SushiChan.t.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "ds-test/test.sol";
+import "./../SushiChan.sol";
+import "./utils/User.sol";
+
+contract SushiChanTest is DSTest {
+    SushiChan internal sushiChan;
+    User internal owner;
+    User internal user;
+    string internal prefixURI = "ipfs://xxxxxxxxxxxxxxxxxxxxxxxx/";
+
+    function setUp() public {
+        sushiChan = new SushiChan(
+            0x3d2341ADb2D31f1c5530cDC622016af293177AE0, //vrf
+            0xb0897686c545045aFc77CF20eC7A532E3120E0F1, //link token
+            prefixURI
+        );
+
+        owner = new User(sushiChan);
+        user = new User(sushiChan);
+
+        sushiChan.transferOwnership(address(owner));
+    }
+
+    function test_mint() public {
+        uint256 prevBalance = sushiChan.balanceOf(address(owner));
+        owner.mint();
+        uint256 postBalance = sushiChan.balanceOf(address(owner));
+        assertEq(prevBalance + 1, postBalance);
+    }
+
+    function testFail_mint_not_owner() public {
+        user.mint();
+    }
+
+    function testFail_mint_more_than_max_supply() public {
+        for (uint256 index = 0; index < sushiChan.MAX_SUPPLY() + 2; index++) {
+            owner.mint();
+        }
+    }
+
+    function test_mintMultiple() public {
+        uint256 prevBalance = sushiChan.balanceOf(address(owner));
+        owner.mintMultiple();
+        uint256 postBalance = sushiChan.balanceOf(address(owner));
+        assertEq(prevBalance + 2, postBalance);
+    }
+
+    function testFail_mintMultiple_not_owner() public {
+        user.mintMultiple();
+    }
+
+    function test_setPrefixURI() public {
+        owner.setPrefixURI("new link");
+        assertEq(sushiChan.prefixURI(), "new link");
+    }
+
+    function testFail_setPrefixURI_not_owner() public {
+        user.setPrefixURI("this will revert");
+    }
+
+    function test_tokenURI_random_not_set() public {
+        owner.mint();
+        assertEq(sushiChan.tokenURI(sushiChan.totalSupply() - 1), string(abi.encodePacked(prefixURI, "nrs")));
+    }
+
+}
---
+++ src/test/utils/User.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "./../../SushiChan.sol";
+import "@openzeppelin/contracts/token/erc721/IERC721Receiver.sol";
+
+contract User is IERC721Receiver {
+    SushiChan internal sushiChan;
+
+    constructor(SushiChan _sushiChan) {
+        sushiChan = _sushiChan;
+    }
+
+    function mint() public {
+        sushiChan.mint(address(this));
+    }
+    
+    function mintMultiple() public {
+        address[] memory addresses = new address[](2);
+        addresses[0] = address(this);
+        addresses[1] = address(this);
+        sushiChan.mintMultiple(addresses);
+    }
+
+    function withdrawLinkTokens() public {
+        sushiChan.withdrawLinkTokens();
+    }
+
+    function setPrefixURI(string memory _prefixURI) public {
+        sushiChan.setPrefixURI(_prefixURI);
+    }
+
+    function getRandomNumber() public {
+        sushiChan.getRandomNumber();
+    }
+
+    function onERC721Received(
+        address operator,
+        address from,
+        uint256 tokenId, 
+        bytes calldata data
+    ) external pure override returns (bytes4) {
+        return IERC721Receiver.onERC721Received.selector;
+    }
+
+}
